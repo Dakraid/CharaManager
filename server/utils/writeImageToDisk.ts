173eae258d4e8545f26692ce24b3ab2ba72f6232
Kristan Schlikow <kristian@schlikow.de>
2024-05-25 01:22:33 +0200

    feat: Ensure correct handling for base64 image content

--- server/utils/writeImageToDisk.ts
+++ server/utils/writeImageToDisk.ts
@@ -2,6 +2,10 @@ import Jimp from 'jimp-compact';
 import fs from 'node:fs';
 
 export default async function writeImageToDisk(id: number, base64Image: string) {
+    if (base64Image.includes('base64,')) {
+        base64Image = base64Image.split('base64,')[1];
+    }
+
     if (!fs.existsSync('public/cards/')) {
         fs.mkdirSync('public/cards/');
     }
