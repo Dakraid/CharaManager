3c1051c9b6fc695880f0e71c72c4cc1cc3f8bb90
Kristan Schlikow <kristian@schlikow.de>
2024-05-31 21:03:57 +0200

    feat: Add stealth plugin to Playwright to avoid being blocked

--- server/api/janitoraime.post.ts
+++ server/api/janitoraime.post.ts
@@ -1,10 +1,34 @@
 import fs from 'node:fs';
 import playwright from 'playwright';
+import { addExtra } from 'playwright-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import ApiResponse from '~/models/ApiResponse';
 import StatusCode from '~/models/enums/StatusCode';
 
-// noinspection JSUnusedGlobalSymbols
+async function setupPlaywrightChromium() {
+    const chromium = addExtra(playwright.chromium);
+    chromium.use(StealthPlugin());
+    // For some reason dependency resolution fails by default, so we import the defaults here manually
+    chromium.plugins.setDependencyResolution('stealth/evasions/chrome.app', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/chrome.csi', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/chrome.loadTimes', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/chrome.runtime', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/defaultArgs', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/iframe.contentWindow', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/media.codecs', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/navigator.hardwareConcurrency', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/navigator.languages', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/navigator.permissions', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/navigator.plugins', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/navigator.webdriver', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/sourceurl', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/user-agent-override', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/webgl.vendor', StealthPlugin);
+    chromium.plugins.setDependencyResolution('stealth/evasions/window.outerdimensions', StealthPlugin);
+    return await chromium.launch({ headless: true });
+}
 
+// noinspection JSUnusedGlobalSymbols
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig(event);
 
@@ -22,13 +46,14 @@ export default defineEventHandler(async (event) => {
     const characterFilename = body.characterUrl.split('characters/')[1];
 
     try {
-        const browser = await playwright.chromium.launch({ headless: true });
+        const browser = await setupPlaywrightChromium();
         const context = await browser.newContext({ acceptDownloads: true });
         const page = await context.newPage();
         await page.goto(characterUrl);
         await page.click('button:has-text("Download")');
         const download = await page.waitForEvent('download');
         await download.saveAs(`./temp/${characterFilename}.png`);
+        await browser.close();
     } catch (err) {
         event.context.logger.error(err);
         return new ApiResponse(StatusCode.INTERNAL_SERVER_ERROR, 'Error occurred while trying to download from JanitorAI.me', err);
